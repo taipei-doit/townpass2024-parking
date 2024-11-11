a3757a83e285aedec07f47c03f37eeaa793ed59f
WavJaby <WavJaby@gmail.com>
2024-11-11 15:54:03 +0800

    Remove unused code

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,14 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
-import FormView from '../views/FormView.vue';
-import SurroundingServiceView from '../views/SurroundingServiceView.vue';
-import TopQnAListView from '../views/TopQnAListView.vue';
-import QnAListView from '../views/QnAListView.vue';
-import CouponView from '../views/CouponView.vue';
-import CouponListView from '../views/CouponListView.vue';
-import HotSpotView from '../views/HotSpotView.vue';
-import TicketDetail from '../views/TicketDetail.vue';
-import CouponDetail from '../views/CouponDetail.vue';
 import ParkingService from '@/views/ParkingService.vue';
 
 const router = createRouter({
@@ -18,56 +8,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: ParkingService
-    },
-    {
-      path: '/form/:id',
-      name: 'form',
-      component: FormView
-    },
-    {
-      path: '/surrounding-service',
-      name: 'surrounding-service',
-      component: SurroundingServiceView
-    },
-    {
-      path: '/qna',
-      name: 'top-qna-list',
-      component: TopQnAListView
-    },
-    {
-      path: '/qna/categories',
-      name: 'qna-list',
-      component: QnAListView
-    },
-    {
-      path: '/coupon',
-      children: [
-        {
-          path: '',
-          name: 'coupon',
-          component: CouponView
-        },
-        {
-          path: 'list',
-          name: 'coupon-list',
-          component: CouponListView
-        },
-        {
-          path: 'hotspot/:id',
-          name: 'hotspot',
-          component: HotSpotView
-        },
-        {
-          path: 'ticket-detail/:id',
-          name: 'ticket-detail',
-          component: TicketDetail
-        },
-        {
-          path: 'coupon-detail/:id',
-          name: 'coupon-detail',
-          component: CouponDetail
-        }
-      ]
     }
   ]
 });
